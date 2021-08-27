1cb4dcfdbc0542a2df7535f95c03c3b0ff590e31
leyuri <leyuri97@gmail.com>
2021-08-27 22:50:10 +0900

    Move the router object to a separate file (router/index.js)

--- src/main.js
+++ src/main.js
@@ -1,23 +1,5 @@
 import Vue from 'vue'
-import App from './App.vue'
-import VueRouter from 'vue-router'
-
-Vue.use(VueRouter)
-
-const Login = { template: '<div>Login Page</div>' }
-
-const NotFound= { template: '<div>Page out found</div>' }
-
-const routes = [
-  { path: '/', component: App },
-  { path: '/login', component: Login },
-  { path: '*', component: NotFound }
-]
-
-const router = new VueRouter({
-  mode: 'history',
-  routes // short for `routes: routes`
-})
+import router from './router/index'
 
 new Vue({
   el: '#app',
