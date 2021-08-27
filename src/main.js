e52458b3bc59a7339cb5ecbb46d2cc97cbc66172
leyuri <leyuri97@gmail.com>
2021-08-27 22:44:50 +0900

    Add a router using vue-router

--- src/main.js
+++ src/main.js
@@ -1,23 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
 
 const Login = { template: '<div>Login Page</div>' }
 
 const NotFound= { template: '<div>Page out found</div>' }
 
-const routes = {
-  '/': App,
-  '/login': Login
-}
+const routes = [
+  { path: '/', component: App },
+  { path: '/login', component: Login },
+  { path: '*', component: NotFound }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes // short for `routes: routes`
+})
 
 new Vue({
   el: '#app',
-  computed: {
-    VueComponent() {
-      return routes[window.location.pathname] || NotFound  ;
-    }
-  },
-  render(h) {
-    return h(this.VueComponent)
-  }
+  router,
+  render: h => h({template: '<router-view></router-view>'})
 })
