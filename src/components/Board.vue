bb8568b6cdab82baa82f6a45de3b19f93faddce2
leyuri <leyuri97@gmail.com>
2021-08-28 11:42:08 +0900

    Add data patch method for Board, Card Component

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,11 +1,14 @@
 <template>
     <div>
         Board
-        <div>bid: {{bid}}</div>
-        <hr />
-        <router-link :to="`/b/${bid}/c/1`">Card 1</router-link>
-        <router-link :to="`/b/${bid}/c/2`">Card 2</router-link>
-        <router-view></router-view>
+        <div v-if="loading">loading board...</div>
+        <div v-else>
+            <div>bid: {{bid}}</div>
+            <hr />
+            <router-link :to="`/b/${bid}/c/1`">Card 1</router-link>
+            <router-link :to="`/b/${bid}/c/2`">Card 2</router-link>
+            <router-view></router-view>
+        </div>
     </div>
 </template>
 
@@ -13,11 +16,22 @@
 export default {
     data() {
         return {
-            bid: 0
+            bid: 0,
+            loading: false
         }
     },
     created() {
-        this.bid = this.$route.params.bid
+        this.featchData()
+        
+    },
+    methods: {
+        featchData() {
+            this.loading = true
+            setTimeout(() => {
+                this.bid = this.$route.params.bid
+                this.loading = false
+            }, 500)
+        }
     }
 }
 </script>
