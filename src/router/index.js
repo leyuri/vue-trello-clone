d008c4773cd255aa1a1a9e17bd65f80796921310
leyuri <leyuri97@gmail.com>
2021-08-28 09:17:28 +0900

    Add router after creating Board Component, parameter access for dynamic routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../components/Home.vue'
+import Board from '../components/Board.vue'
 import Login from '../components/Login.vue'
 import NotFound from '../components/NotFound.vue'
 
@@ -9,6 +10,7 @@ Vue.use(VueRouter)
 const routes = [
   { path: '/', component: Home },
   { path: '/login', component: Login },
+  { path: '/b/:bid', component: Board },
   { path: '*', component: NotFound }
 ]
 
