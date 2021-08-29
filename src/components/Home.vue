bd3e80f322112383928cf00a703654b2d6768a15
leyuri <leyuri97@gmail.com>
2021-08-29 16:27:26 +0900

    Apply vuex to Home and AddBoard Component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -23,11 +23,7 @@
         </a>
       </div>
     </div>
-    <AddBoard
-      v-if="isAddBoard"
-      @close="isAddBoard = false"
-      @submit="onAddBoard"
-    />
+    <AddBoard v-if="isAddBoard" />
   </div>
 </template>
 
@@ -35,7 +31,7 @@
 <script>
 import { board } from "../api";
 import AddBoard from "./AddBoard.vue";
-import { mapState, mapMutations } from "vuex";
+import { mapState, mapMutations, mapActions } from "vuex";
 
 export default {
   components: {
@@ -44,12 +40,14 @@ export default {
   data() {
     return {
       loading: false,
-      boards: [],
       error: "",
     };
   },
   computed: {
-    ...mapState(["isAddBoard"]),
+    ...mapState({
+      isAddBoard: "isAddBoard",
+      boards: "boards",
+    }),
   },
   created() {
     this.fetchData();
@@ -61,19 +59,13 @@ export default {
   },
   methods: {
     ...mapMutations(["SET_IS_ADD_BOARD"]),
+    ...mapActions(["FETCH_BOARDS"]),
+
     fetchData() {
       this.loading = true;
-      board
-        .fetch()
-        .then((data) => {
-          this.boards = data.list;
-        })
-        .finally((_) => {
-          this.loading = false;
-        });
-    },
-    onAddBoard() {
-      this.fetchData();
+      this.FETCH_BOARDS().finally((_) => {
+        this.loading = false;
+      });
     },
   },
 };
