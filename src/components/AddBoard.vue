c7ef89829187625a7d4323168ff18067b28782f6
leyuri <leyuri97@gmail.com>
2021-08-30 20:52:07 +0900

    Fix the code order

--- src/components/AddBoard.vue
+++ src/components/AddBoard.vue
@@ -56,10 +56,10 @@ export default {
     ...mapMutations(["SET_IS_ADD_BOARD"]),
     ...mapActions(["ADD_BOARD", "FETCH_BOARDS"]),
     addBoard() {
-      this.SET_IS_ADD_BOARD(false);
-      this.ADD_BOARD({ title: this.input }).then(({ id }) => {
-        this.$router.push(`/b/${id}`);
-      });
+      this.ADD_BOARD({ title: this.input })
+        .then(({ id }) => this.$router.push(`/b/${id}`))
+        .catch((err) => console.error(err))
+        .finally(() => this.SET_IS_ADD_BOARD(false)); // 무조건 실행될 코드가 위치
     },
   },
 };
