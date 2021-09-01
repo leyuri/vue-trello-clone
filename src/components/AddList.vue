6858851d5cbe8f5896768e6f4c71b34ecbbd7e50
leyuri <leyuri97@gmail.com>
2021-09-01 22:30:57 +0900

    Add blur, keyup event & List style

--- src/components/AddList.vue
+++ src/components/AddList.vue
@@ -6,8 +6,10 @@
       class="form-control"
       v-model="inputTitle"
       ref="inputTitle"
+      @blur="restore"
+      @keyup.enter="onSubmitTitle"
     />
-    <a v-else href="" @click.prevent="onAddList">&plus; Add a list</a>
+    <a v-else href="" @click.prevent="onAddList">&plus; Add a list...</a>
   </div>
 </template>
 
@@ -24,9 +26,31 @@ export default {
       this.isAddList = true;
       this.$nextTick(() => this.$refs.inputTitle.focus());
     },
+    onSubmitTitle() {
+      console.log("enter!!!");
+    },
+    restore() {
+      this.isAddList = false;
+      this.inputTitle = "";
+    },
   },
 };
 </script>
 
-<style >
+<style>
+.add-list {
+  background-color: rgba(0, 0, 0, 0.1);
+  padding: 10px;
+  color: #ddd;
+  transition: all 0.3s;
+}
+.add-list a {
+  color: #ddd;
+  text-decoration: none;
+}
+
+.add-list:hover,
+.add-list:focus {
+  background-color: rgba(0, 0, 0, 0.3);
+}
 </style>
